7e33755f3be9b070e52ffbc8475ba91f731412a0
zihad868 <zihadh868@gmail.com>
2024-08-27 00:18:55 +0600

    Connect MongoDB

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,8 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:<db_password>@cluster0.7lbrva6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7lbrva6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -11,16 +13,15 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+async function connectDB(){
+   try{
+     await client.connect();
+     console.log("MongoDB Connect Success");
+     return client.db('MarketingBlog')
+   }catch(error){
+     console.error('MongoDB Connection Error', error);
+     process.exit(1);
+   }
 }
-run().catch(console.dir);
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+//DB Connect
+const mongodbConnection = require('./Config/db');
+mongodbConnection();
+
+
 app.get('/', (req, res) =>{
     res.send("Server is running ...")
 })
-
-console.log(process.env.DB_USER)
-
 
 app.listen(port, () =>{
     console.log(`Server running on port ${port}`)
