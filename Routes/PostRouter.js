37882ce98938c7288f0af031d7741e21c5d7d380
zihad868 <zihadh868@gmail.com>
2024-09-05 12:27:07 +0600

    Authentic User Can Post Data

--- Routes/PostRouter.js
+++ Routes/PostRouter.js
@@ -1,11 +1,12 @@
 const { model } = require('mongoose');
 const { post } = require('../Controllers/PostControllers');
 const { PostValidation } = require('../Middlewares/PostValidation');
+const ensureAuthentication = require('../Middlewares/VerifyUser');
 
 const router = require('express').Router();
 
 
-router.post('/post', PostValidation, post);
+router.post('/post', ensureAuthentication, PostValidation, post);
 
 
 module.exports = router;
